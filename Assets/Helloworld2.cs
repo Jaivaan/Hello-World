8200c7cc336cd3205851c87c9025bc3a0b216be8
Jaivaan <ivan.renteria@opendeusto.es>
2024-02-23 08:49:50 +0100

    Nuevo Helloworld2

---
+++ Assets/Helloworld2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Juego{
+public class Helloworld2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+}
+
